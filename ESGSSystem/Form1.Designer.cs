8d010aa2243ad445d5c0345c26b8ca74ad25f7bf
Deli Kristóf <delikristof@gmail.com>
2015-06-11 19:48:32 +0200

    Libraries + Connection

--- ESGSSystem/Form1.Designer.cs
+++ ESGSSystem/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.label4 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
             this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -155,13 +156,22 @@
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(305, 328);
             this.textBox5.TabIndex = 12;
-            this.textBox5.Text = "asdas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(289, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "BOT Log";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(602, 373);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.label4);
@@ -197,6 +207,7 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- ESGSSystem/Form1.cs
+++ ESGSSystem/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using QueryMaster;
+using AServerQuery;
 
 namespace ESGSSystem
 {
@@ -16,12 +17,15 @@ namespace ESGSSystem
         public Form1()
         {
             InitializeComponent();
+           //logs.Say += new EventHandler<ChatEventArgs>(logs_Say);
         }
+        //var listener = new LogListener();
         string ip = "";
         ushort port = 0;
         string rconpw = "";
-        Server server = ServerQuery.GetServerInstance(EngineType.Source, "", 0);
+        Server server = null;
         Logs logs = null;
+
         
         private void button4_Click(object sender, EventArgs e)
         {
@@ -29,37 +33,45 @@ namespace ESGSSystem
             port = Convert.ToUInt16(textBox2.Text);
             rconpw = textBox3.Text;
 
-            Server server = ServerQuery.GetServerInstance(EngineType.Source, ip, port);
+            server = ServerQuery.GetServerInstance(EngineType.Source, ip, port);
             logs = server.GetLogs(port);
-            ServerInfo info = server.GetInfo();
-            textBox5.AppendText(textBox5.Text + "n/ Current map: " + info.Map);
+            QueryMaster.ServerInfo info = server.GetInfo();
+            sendLog("Current Map: " + info.Map);
+            button4.Enabled = false;
 
         }
         //logs.Say += new EventHandler<ChatEventArgs>(logs_Say);
         private void textBox1_TextChanged(object sender, EventArgs e)
-        { 
-
+        {
+            
+        }
+        public void sendLog(string msg)
+        {
+            textBox5.AppendText("\n" + msg);
         }
         public void rconcmd(string cmd)
         {
             Rcon rcon = server.GetControl(rconpw);
             if (rcon == null)
             {
-                //msgbox.Show("Invalis PW");
+                MessageBox.Show("Invalid PW");
             }
             else
             {
             //send server commmand
-            string serverResponse= rcon.SendCommand("cmd");
+            string serverResponse= rcon.SendCommand(cmd);
+                sendLog("RCON Command " + cmd + "has sent");
             }           
         }
         public void say(string msg)
         {
             rconcmd("say [ESGS] " + msg);
+            
         }
         private void button2_Click(object sender, EventArgs e)
         {
             rconcmd("exec esl5on5");
+
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -69,11 +81,39 @@ namespace ESGSSystem
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             say("Match Started!");
-            while (true)
+            MessageBox.Show("DEBUG 1");
+            saylisten();
+
+        }
+        public void saylisten()
+        {
+            while(true)
+            {
+                logs.Say += new EventHandler<ChatEventArgs>(logs_Say);
+            }
+            
+
+        }
+        static void logs_Say(object sender, ChatEventArgs e)
+        {
+            MessageBox.Show("DEBUG 2");
+            //sendLog("Sender :");
+            Console.WriteLine("\tName : " + e.Player.Name);
+            Console.WriteLine("\tUid : " + e.Player.Uid);
+            Console.WriteLine("\tWonid : " + e.Player.WonId);
+            Console.WriteLine("\tTeam : " + e.Player.Team);
+            Console.WriteLine("Message : " + e.Message);
+            Console.WriteLine("Timestamp : " + e.Timestamp);
+            
+            if(e.Message == "!ready")
             {
 
             }
+          
         }
+        //logs.Say += new EventHandler<ChatEventArgs>(logs_Say);
+
     }
 }
